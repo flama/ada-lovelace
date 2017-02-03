3e4cc6fa18a7235e655549f6fccfcf10229280d2
Yurick <yurick@flama.me>
2017-02-03 17:33:12 -0200

    Selects international feminists

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import himalaya from 'himalaya'
+
 import 'whatwg-fetch'
 import './App.scss'
 
 const wikipedia = {
-  english: 'https://en.wikipedia.org/w/api.php?action=parse&page=List_of_feminists&format=json'
+  english: 'https://en.wikipedia.org/w/api.php?action=parse&page=List_of_feminists&format=json&origin=*'
 }
 
 class App extends Component {
@@ -11,13 +13,22 @@ class App extends Component {
   render() {
     let request = new Request(wikipedia.english, {
       header: new Headers({
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Origin': 'http://adalovelace.flama.me'
+        'Content-Type': 'application/json; charset=UTF-8'
       })
     })
 
     window.fetch(request)
-    .then(response => console.log(response))
+      .then(response => response.json())
+      .then(json => json.parse.text['*'])
+      .then(html => himalaya.parse(html))
+      .then(tables => tables.filter(table =>
+        table.tagName === "table" &&
+        typeof table.attributes !== "undefined" &&
+        table.attributes.border === 1
+      ))
+      .then(wikitables => console.log(wikitables))
+      .catch(error => console.error(error))
+
     return (<div/>)
   }
 }
