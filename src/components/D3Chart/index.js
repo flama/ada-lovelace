9fd2d4b0d43618160faf06ddd5791eda93db8c1f
anapaulavnoronha <anapaulavnoronha@gmail.com>
2017-02-17 15:10:21 -0200

    Adds D3Chart helper

---
+++ src/components/D3Chart/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import d3chart from './d3chart'
+
+import './styles.scss'
+
+class D3Chart extends Component {
+  render() {
+    return (<div />)
+  }
+}
+
+export default D3Chart
