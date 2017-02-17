a5ad461716fefd9a1609efaa0f08529f48c62806
anapaulavnoronha <anapaulavnoronha@gmail.com>
2017-02-17 15:22:59 -0200

    Creates D3Chart component

--- src/components/D3Chart/index.js
+++ src/components/D3Chart/index.js
@@ -4,9 +4,40 @@ import d3chart from './d3chart'
 import './styles.scss'
 
 class D3Chart extends Component {
-  render() {
-    return (<div />)
+
+  componentDidMount = () => {
+    let el = this.getDOMNode()
+    d3chart(el,{
+      width: "100%",
+      height: "300px"
+    }, this.getChartState())
   }
+
+  componentDidUpdate = () => {
+    let el = this.getDOMNode()
+    d3chart.update(el,this.getChartState())
+  }
+
+  getChartState = () => {
+    return {
+      data: this.props.data,
+      domain: this.props.domain
+    }
+  }
+
+  componentWillMount = () => {
+    let el = this.getDOMNode();
+    d3chart.destroy(el);
+  }
+
+  render() {
+    return (<div className="Chart" />)
+  }
+}
+
+D3Chart.propTypes = {
+  data: React.PropTypes.array,
+  domain: React.PropTypes.object
 }
 
 export default D3Chart
