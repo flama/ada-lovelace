1fa00f90f5dc8b1486788e1e9ee23b34d874e318
Yurick <yurick@flama.me>
2017-02-17 17:29:03 -0200

    Fixes App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import FilterableDataVis from '../FilterableDataVis'
 import DataLoader from '../DataLoader'
-<<<<<<< HEAD
+
 import D3Chart from '../D3Chart'
 
 import './App.scss'
@@ -29,36 +29,13 @@ class App extends Component {
     this.setState({ dataList })
   }
 
-=======
-
-import './App.scss'
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      dataList: []
-    }
-
-    this.fetchData = this.fetchData.bind(this)
-  }
-
-  fetchData(dataList) {
-    this.setState({ dataList })
-  }
-
->>>>>>> a5ad461716fefd9a1609efaa0f08529f48c62806
   render()
   {
     return (
       <section>
         <DataLoader fetchData={this.fetchData} />
         <FilterableDataVis dataList={this.state.dataList} />
-<<<<<<< HEAD
         <D3Chart data={this.state.data} domain={this.state.domain} />
-=======
->>>>>>> a5ad461716fefd9a1609efaa0f08529f48c62806
       </section>
     )
   }
