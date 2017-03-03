f5fabde306806fdef5c014a36b2a8f2f23682053
Yurick <yurick@flama.me>
2017-03-02 21:00:00 -0300

    Changes to actual data

--- src/components/ScatterPlot/index.js
+++ src/components/ScatterPlot/index.js
@@ -4,14 +4,13 @@ import ScatterRow from '../ScatterRow'
 import './styles.scss'
 
 const domain = {
-  x: [1700, 2030]
+  x: [1800, 2030]
 }
 
 class ScatterPlot extends Component {
 
   render() {
 
-    //TODO: change to actual data
     let topics = Object.keys(this.props.dataList).map(category => {
       return (<ScatterRow
         data={ this.props.dataList[category] }
