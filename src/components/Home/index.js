228997469b4f9903052a9e3e0eeddc44bcddeb5c
Yurick <yurick@flama.me>
2017-03-10 15:26:14 -0300

    Create NavBar component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import NavBar from '../NavBar'
 
 import arrowDown from './arrow-down.svg'
 import './styles.scss'
@@ -8,17 +9,7 @@ class Home extends Component {
   render() {
     return (
       <section id="home">
-        <div className="header">
-          <a href="#" className="noink left">
-            made with ♥ by flama
-          </a>
-          <a href="#" className="noink right">
-            about
-          </a>
-          <a href="#" className="noink right">
-            contributing
-          </a>
-        </div>
+        <NavBar current="home" />
         <div className="body">
           <h1>wikimina</h1>
           <p>Take a quick look around you. The world as you know has been crafted by both men and women — but all you know is about the former ones. Now, think about your work or your hobbies. How many women are a reference in those fields? For how long have you known them? How hard was for them to be accepted or recognized?</p>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import './styles.scss'
+
+class NavBar extends Component {
+
+  classesFor = (route, position) => {
+    return `noink ${position} ${ route === this.props.current? "current":""}`
+  }
+
+  render() {
+    return (
+      <div id="navbar">
+        <a href="#" className={ this.classesFor("home", "left") }>
+          made with ♥ by flama
+        </a>
+        <a href="#" className={ this.classesFor("about", "right") }>
+          about
+        </a>
+        <a href="#" className={ this.classesFor("contributing", "right")}>
+          contributing
+        </a>
+      </div>
+    )
+  }
+}
+
+NavBar.propTypes = {
+  current: React.PropTypes.oneOf([ "home", "about", "contributing" ])
+}
+
+export default NavBar
