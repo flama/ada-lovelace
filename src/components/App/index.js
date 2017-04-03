a6663d04c37b43138db7d5bcf9c326827551ddcb
anapaulavnoronha <anapaulavnoronha@gmail.com>
2017-04-03 11:59:07 -0300

    Add Share component structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import GraphPage from '../GraphPage'
 import Home from '../Home'
+import Share from '../Share'
 
 import './App.scss'
 
@@ -12,6 +13,7 @@ class App extends Component {
       <div id="app">
         <Home />
         <GraphPage />
+        <Share />
       </div>
     )
   }
---
+++ src/components/Share/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import './style.scss'
+
+class Share extends Component {
+
+  render()
+  {
+    return (
+      <div id="share">
+        <h3>Share</h3>
+        <p>We envision an ever-growing platform, crowdsourced by people everywhere, helping us show women who somehow have created an impact in our world! Until then, you can help us by spreading the word of Wikimina :)</p>
+      </div>
+    )
+  }
+}
+
+export default Share;
