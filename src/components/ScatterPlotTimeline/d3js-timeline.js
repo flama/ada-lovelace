075e451a1e3d9f4f6a92da41a9d95a1add618c3d
Ralph Effting <ralfting@gmail.com>
2017-04-06 14:51:41 -0300

    Timeline (#27)
    * Create component timeline
    * Fix format Year
    * Change timeline component colors
    * Add space between points and line

---
+++ src/components/ScatterPlotTimeline/d3js-timeline.js
@@ -0,0 +1,35 @@
+import * as d3 from 'd3'
+
+export default
+class d3TimelineHelper {
+
+  constructor(el, props, state) {
+    let svg = d3.select(el).append('svg')
+      .attr('width', '100%')
+      .attr('height', '60px')
+      .attr('background', '#000')
+
+    let x = this._scaleX(el, state.domain)
+
+    this._draw(svg, state.domain, x);
+  }
+
+  _scaleX(el, domain) {
+
+    if(!domain) return null
+
+    let width = el.offsetWidth
+
+    let x = d3.scaleTime()
+      .range([0, width])
+      .domain([new Date(domain.x[0], 1, 1), new Date(domain.x[1], 1, 1)])
+
+    return x
+  }
+
+  _draw(svg, domain, x) {
+
+    let g = svg.append("g")
+      .call(d3.axisBottom(x).tickSizeOuter(0).ticks(25).tickPadding(12));
+  }
+}
