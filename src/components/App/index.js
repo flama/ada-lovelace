1871361dc44a3ce3e8d4ee6791d81f69ad218d73
Yurick <yurick@flama.me>
2017-04-07 14:30:54 -0300

    Add image to share

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 import Home from '../Home'
 import GraphPage from '../GraphPage'
 import AboutPage from '../AboutPage'
 import Share from '../Share'
 import Footer from '../Footer'
+
+import shareImage from './wikimina.png'
+
 
 import './App.scss'
 
@@ -11,8 +15,13 @@ class App extends Component {
 
   render()
   {
+    let host = "http://flama.github.io/ada-lovelace"
     return (
       <div id="app">
+        <Helmet>
+          <meta property="og:image" content={ `${host}${shareImage}` } />
+          <meta property="og:image:secure_url" content={ `${host}${shareImage}` } />
+        </Helmet>
         <Home />
         <GraphPage />
         <AboutPage />
