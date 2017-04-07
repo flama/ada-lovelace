3dc763082733cf8f43e55fad1b6f27dda9fad220
anapaulavnoronha <anapaulavnoronha@gmail.com>
2017-04-07 11:36:41 -0300

    Add share buttons

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import { Element } from 'react-scroll'
+import { ShareButtons } from 'react-share'
+import postimage from '../../../public/home.png'
 
 import './style.scss'
+
+const {
+  FacebookShareButton,
+  TwitterShareButton
+} = ShareButtons;
+
 
 class Share extends Component {
 
   render()
   {
+
+    console.log(postimage)
     return (
       <Element name="share" id="share">
         <div className="content">
@@ -14,8 +24,8 @@ class Share extends Component {
           <p>We envision an ever-growing platform, crowdsourced by people everywhere, helping us show women who somehow have created an impact in our world!
            Until then, you can help us by spreading the word of Wikimina :)</p>
            <div className="actions">
-             <button>Facebook</button>
-             <button>Twitter</button>
+             <FacebookShareButton url="https://flama.github.io/ada-lovelace/" description="Inspiring and helping create representativeness so that more women become awesome!" picture={`https://flama.github.io/ada-lovelace${postimage}`} >Facebook</FacebookShareButton>
+             <TwitterShareButton url="https://flama.github.io/ada-lovelace/" title="Wikimina" via="studioflama" hashtags={['wikimina', 'grrlpower', 'whyhistorymatters', 'empoweringwomen']} >Twitter</TwitterShareButton>
           </div>
         </div>
       </Element>
