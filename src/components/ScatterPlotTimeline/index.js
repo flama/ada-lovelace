c5c61ce4855fd54c5f95e95b280cf988ddafc276
Yurick <yurick@flama.me>
2017-04-07 09:55:47 -0300

    Refactor timeline (#42)

--- src/components/ScatterPlotTimeline/index.js
+++ src/components/ScatterPlotTimeline/index.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react'
+import d3TimelineHelper from './d3js-timeline'
 
 import './styles.scss'
-import d3TimelineHelper from './d3js-timeline'
 
 class GraphTimeline extends Component {
   componentDidMount = () => {
-    this.d3TimelineHelper = new d3TimelineHelper(this.timeline, {
-      width: "100%",
-      height: "100%"
-    }, this.getChartState())
-  }
-
-  getChartState = () => {
-    return {
-      domain: this.props.domain
-    }
+    this.d3TimelineHelper = new d3TimelineHelper(this.timeline, this.props)
   }
 
   render() {
