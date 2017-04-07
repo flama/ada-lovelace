afefea461181062a9bcbafcfb7c85bf094ebf989
Yurick <yurick@flama.me>
2017-04-07 14:49:22 -0300

    Remove helmet dependency (#52)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
-import { Helmet } from 'react-helmet'
 import Home from '../Home'
 import GraphPage from '../GraphPage'
 import AboutPage from '../AboutPage'
 import Share from '../Share'
 import Footer from '../Footer'
-
-import shareImage from './wikimina.png'
-
 
 import './App.scss'
 
@@ -18,10 +14,6 @@ class App extends Component {
     let host = "http://flama.github.io/ada-lovelace"
     return (
       <div id="app">
-        <Helmet>
-          <meta property="og:image" content={ `${host}${shareImage}` } />
-          <meta property="og:image:secure_url" content={ `${host}${shareImage}` } />
-        </Helmet>
         <Home />
         <GraphPage />
         <AboutPage />
