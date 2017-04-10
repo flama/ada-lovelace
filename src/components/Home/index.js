7e0e7615b3d384c418a9efc99d722b903f3e7588
Ana Paula <anapaula@flama.me>
2017-04-10 15:13:53 -0300

    Update text in Home component (#62)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,10 +11,9 @@ class Home extends Component {
         <NavBar current="home" />
         <div className="body">
           <h1>wikimina</h1>
-          <p>{ `Take a quick look around you. The world as you know has been crafted by both men
-            and women — but all you know is about the former ones. Now, think about your work or
-            your hobbies. How many women are a reference in those fields? For how long have you
-            known them? How hard was for them to be accepted or recognized?` }</p>
+          <p>{ `Both men and women have crafted the world as we know it.
+            Now, just for a minute, take a quick look around. Think about your work or your hobbies.
+            How many women are a reference in those fields?` }</p>
         </div>
       </section>
     )
