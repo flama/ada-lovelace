7970ece72d71bf45f1e7e862d86fc497baaaf393
Yurick <yurick@flama.me>
2017-04-12 17:33:19 -0300

    Remove all errors from tests

--- src/components/ExpansibleScatterRow/test.js
+++ src/components/ExpansibleScatterRow/test.js
@@ -1,3 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ExpansibleScatterRow from './ExpansibleScatterRow'
+import ExpansibleScatterRow from '../ExpansibleScatterRow'
+
+test("the truth", () => {
+  expect(true).toBeTruthy()
+})
