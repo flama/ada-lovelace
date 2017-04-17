9af6e6f74ed35cc72deda6c5fbd2dfc6aa8b872e
Yurick <yurick@flama.me>
2017-04-17 16:30:07 -0300

    Finishes testing SelectInput

--- src/components/SelectInput/test.js
+++ src/components/SelectInput/test.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 
 import SelectInput from '../SelectInput'
 
 describe('SelectInput', () => {
+  let selectInput
+  let onChange
+  let closeBubble
+
+  beforeEach(() => {
+    onChange = jest.fn()
+    closeBubble = jest.fn()
+    selectInput = mount(<SelectInput />)
+  })
+
   it('renders correctly', () => {
-    const tree = renderer.create(<SelectInput />).toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(toJSON(selectInput)).toMatchSnapshot()
+  })
+
+  it('calls callbacks when option changes', () => {
+    selectInput.find('select').simulate('change')
+
+    selectInput.setProps({
+      onChange,
+      closeBubble,
+      options: [1, 2, 3]
+    })
+
+    expect(toJSON(selectInput)).toMatchSnapshot()
+
+    selectInput.find('select').simulate('change')
+
+    expect(onChange).toHaveBeenCalled()
+    expect(closeBubble).toHaveBeenCalled()
   })
 })
