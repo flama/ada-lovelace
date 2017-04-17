56bbb1f1a680bbf5eab9cf083abbe63366c962a1
Yurick <yurick@flama.me>
2017-04-17 12:13:49 -0300

    Finish testing of DetailsBubble

--- src/components/DetailsBubble/test.js
+++ src/components/DetailsBubble/test.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 
 import DetailsBubble from '../DetailsBubble'
 
 // TODO: test `close` functionality
 describe('DetailsBubble', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<DetailsBubble />).toJSON()
-    expect(tree).toMatchSnapshot()
+    const tree = shallow(<DetailsBubble />)
+    expect(toJSON(tree)).toMatchSnapshot()
+  })
+
+  it('closes when we ask it to', () => {
+    let close = jest.fn()
+    let detailsBubble = shallow(<DetailsBubble close={ () => close() } />)
+    detailsBubble.find('a.close').simulate('click')
+    expect(close).toHaveBeenCalled()
   })
 })
