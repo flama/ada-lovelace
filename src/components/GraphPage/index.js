17175359185bca5fdf48ef0416c002af5ea01849
Petterson <petterson@flama.me>
2017-04-17 13:34:09 -0300

    Fix problems

--- src/components/GraphPage/index.js
+++ src/components/GraphPage/index.js
@@ -27,7 +27,9 @@ class GraphPage extends Component {
       <section id="graph-page">
         <h3>wonderful women</h3>
         <div className="content">
-          <p>{ `There's a woman behind every single change in our history — whether we know her or not. We have picked a few to try and show it, by sorting them by their influence fields and through time. Take a look and get surprised by how much girl power history can reveal!` }</p>
+          <p>{ `There's a woman behind every single change in our history — whether we know her or not.
+          We have picked a few to try and show it, by sorting them by their influence fields and through
+          time. Take a look and get surprised by how much girl power history can reveal!` }</p>
         </div>
         <div className="wrapper">
           <div className="graph-content">
@@ -41,7 +43,9 @@ class GraphPage extends Component {
             </div>
             <DataLoader fetchData={this.fetchData} />
             <FilterableScatterPlot dataList={this.state.dataList} options={this.state.options} />
-            <p className="information">Information: We collected the information presented from public sources like <a href="https://www.wikipedia.org/" target="_blank">Wikipedia</a> and do not take responsability for the content in the external links.</p>
+            <p className="information">Information: We collected the information presented from public
+            sources like <a href="https://www.wikipedia.org/" target="_blank">Wikipedia</a> and do not take
+            responsability for the content in the external links.</p>
           </div>
         </div>
       </section>
